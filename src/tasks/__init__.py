70441e90d6ffef74750245ee7535f0e25b178872
kevcalhoun <kwcalhou@iu.edu>
2026-02-08 21:51:39 -0600

    register new order processing tasks in Celery

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,15 +1,22 @@
 """Celery tasks for async processing."""
-
 from src.tasks.process_fulfillment import (
     build_fulfillment_chain,
     fulfill_order_task,
     resolve_order_task,
 )
-from src.tasks.process_order import process_shopify_order_task
+from src.tasks.process_order import (
+    fetch_order_data_task,
+    transform_order_task,
+    submit_order_task,
+    build_order_processing_chain,
+)
 
 __all__ = [
     "build_fulfillment_chain",
     "fulfill_order_task",
     "resolve_order_task",
-    "process_shopify_order_task",
+    "fetch_order_data_task",
+    "transform_order_task",
+    "submit_order_task",
+    "build_order_processing_chain",
 ]
